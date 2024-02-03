ffb281a0443319714ac4089a7364316596dd6104
Pacchiotti Marco <57013700+pacchio1@users.noreply.github.com>
2024-02-03 17:42:12 +0100

    restore

--- admin.php
+++
@@ -1,4 +0,0 @@
- <link rel="stylesheet" href="style/style.css">
- <footer style="text-align: right;">
-     <img src="img/pwsLogo.png" alt="PWS Logo" width="50px" height="50px">
- </footer>
--- private/readme.md
+++
@@ -1,1 +0,0 @@
-# do not expose this folder
